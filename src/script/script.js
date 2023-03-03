81b9b123afcc375f8bfc1690ca609fc792db2373
Ailadir <117507118+Ailadir@users.noreply.github.com>
2023-03-03 15:35:02 +0300

    Add new script

--- src/script/script.js
+++ src/script/script.js
@@ -1,13 +1,20 @@
-const choices = ['rock', 'paper', 'scissors'];
-
-function getComputerChoice(choices) {
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
   let randomNumber = Math.floor(Math.random() * choices.length);
   return choices[randomNumber];
 }
 
-let computerChoice = getComputerChoice();
-let playerChoice = prompt('Choose "rock", "paper" or "scissors".');
-
-function playGame(playerChoice, computerChoice) {
-  if (playerChoice == computerChoice) console.log('Draw!');
+function playGame() {
+  let computerChoice = getComputerChoice();
+  let playerChoice = prompt('Choose "rock", "paper" or "scissors".');
+  console.log(playerChoice);
+  console.log(computerChoice);
+  let rules = { rock: 'scissors', paper: 'rock', scissors: 'paper' };
+  if (playerChoice === computerChoice) {
+    console.log('Draw!');
+  } else if (computerChoice === rules[playerChoice]) {
+    console.log('You won!');
+  } else {
+    console.log('You lost!');
+  }
 }
