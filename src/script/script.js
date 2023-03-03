8a0b4c62832de217b48bde0eb4307ce2f01c9aad
Ailadir <117507118+Ailadir@users.noreply.github.com>
2023-03-03 15:45:29 +0300

    Add new function

--- src/script/script.js
+++ src/script/script.js
@@ -1,20 +1,28 @@
 function getComputerChoice() {
+  //Function to get random value for computer choice
   const choices = ['rock', 'paper', 'scissors'];
   let randomNumber = Math.floor(Math.random() * choices.length);
   return choices[randomNumber];
 }
 
-function playGame() {
-  let computerChoice = getComputerChoice();
-  let playerChoice = prompt('Choose "rock", "paper" or "scissors".');
-  console.log(playerChoice);
-  console.log(computerChoice);
-  let rules = { rock: 'scissors', paper: 'rock', scissors: 'paper' };
-  if (playerChoice === computerChoice) {
-    console.log('Draw!');
-  } else if (computerChoice === rules[playerChoice]) {
-    console.log('You won!');
-  } else {
-    console.log('You lost!');
+function Game() {
+  //Function to loop game, before someone score 5 time
+
+  function playGame(playerChoice, computerChoice) {
+    //Function to measure who win and lost(player vs computer)
+    let rules = { rock: 'scissors', paper: 'rock', scissors: 'paper' };
+    if (playerChoice === computerChoice) {
+      return 'Draw!';
+    } else if (computerChoice === rules[playerChoice]) {
+      return 'You won!';
+    } else {
+      return 'You lost!';
+    }
   }
+
+  //Variables to store computer and player choices
+  let computerChoice = getComputerChoice();
+  let playerChoice = prompt('Choose "rock", "paper" or "scissors".').toLowerCase();
+  //Little code for check result
+  console.log(playGame(playerChoice, computerChoice));
 }
