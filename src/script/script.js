9819fc3a9edb4da1bc4906bdda91881ac4919ae3
Ailadir <117507118+Ailadir@users.noreply.github.com>
2023-03-03 16:04:52 +0300

    Improve game script functionality

--- src/script/script.js
+++ src/script/script.js
@@ -5,27 +5,29 @@ function getComputerChoice() {
   return choices[randomNumber];
 }
 
+function playGame(playerChoice, computerChoice) {
+  //Function to measure who win and lost(player vs computer)
+  let playerScore = 0;
+  let computerScore = 0;
+  let rules = { rock: 'scissors', paper: 'rock', scissors: 'paper' };
+  if (playerChoice === computerChoice) {
+    return 'Draw!';
+  } else if (computerChoice === rules[playerChoice]) {
+    return 'You won!';
+    playerScore += 1;
+  } else {
+    return 'You lost!';
+    computerScore += 1;
+  }
+}
 function Game() {
   //Function to loop game, before someone score 5 time
   for (let i = 0; i < 5; i++) {
-    function playGame(playerChoice, computerChoice) {
-      //Function to measure who win and lost(player vs computer)
-      let rules = { rock: 'scissors', paper: 'rock', scissors: 'paper' };
-      if (playerChoice === computerChoice) {
-        // return 'Draw!';
-        console.log('Draw');
-      } else if (computerChoice === rules[playerChoice]) {
-        // return 'You won!';
-        console.log('You won!');
-      } else {
-        console.log('You lost');
-        // return 'You lost!';
-      }
-    }
-
     //Variables to store computer and player choices
     let computerChoice = getComputerChoice();
     let playerChoice = prompt('Choose "rock", "paper" or "scissors".').toLowerCase();
+    let result = playGame(playerChoice, computerChoice);
+    console.log(result);
   }
 }
 
