b96ba79b4e8a41dca7951c739dfdd0ce56e9ba12
Ailadir <117507118+Ailadir@users.noreply.github.com>
2023-03-03 17:49:13 +0300

    Add core game scripts.

--- src/script/script.js
+++ src/script/script.js
@@ -7,27 +7,46 @@ function getComputerChoice() {
 
 function playGame(playerChoice, computerChoice) {
   //Function to measure who win and lost(player vs computer)
-  let playerScore = 0;
-  let computerScore = 0;
   let rules = { rock: 'scissors', paper: 'rock', scissors: 'paper' };
   if (playerChoice === computerChoice) {
     return 'Draw!';
   } else if (computerChoice === rules[playerChoice]) {
     return 'You won!';
-    playerScore += 1;
   } else {
-    return 'You lost!';
-    computerScore += 1;
+    return 'You lost';
   }
 }
+
 function Game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
   //Function to loop game, before someone score 5 time
-  for (let i = 0; i < 5; i++) {
+  while (playerScore < 5 && computerScore < 5) {
     //Variables to store computer and player choices
     let computerChoice = getComputerChoice();
     let playerChoice = prompt('Choose "rock", "paper" or "scissors".').toLowerCase();
+
     let result = playGame(playerChoice, computerChoice);
     console.log(result);
+    if (result === 'You won!') {
+      playerScore++;
+      console.log(`Player score: ${playerScore}, Computer score: ${computerScore}`);
+    } else if (result === 'You lost') {
+      computerScore++;
+      console.log(`Player score: ${playerScore}, Computer score: ${computerScore}`);
+    } else {
+      console.log(`Player score: ${playerScore}, Computer score: ${computerScore}`);
+    }
+  }
+
+  //Determine who won the game
+  if (playerScore > computerScore) {
+    return 'You won the game!';
+  } else if (playerScore < computerScore) {
+    return 'You lost the game!';
+  } else {
+    return 'The game ended in a draw!';
   }
 }
 
